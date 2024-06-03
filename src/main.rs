4c25152540bf328551fdcdff489187a1d30b43b1
Di Wu <cs.wudilet@gmail.com>
2024-06-04 03:07:59 +0800

    program produces an instruction

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,40 @@
+use std::io::{self, Write};
+
+enum TokenKind {
+    TkPunct, // Punctuators
+    TkNum,   // Numeric literals
+    TkEof,   // End-of-file markers
+}
+
+struct Token {
+    kind: TokenKind,
+    next: Box<Node>,
+    val: i32,
+    index: i32,
+    len: i32,
+}
+
+enum Node {
+    Token{
+        kind: TokenKind,
+        next: Box<Node>,
+        val: i32,
+        index: i32,
+        len: i32,
+    },
+    Nil,
+}
+
 fn main() {
-    let mut sum = 0;
-    for n in 1..20 {
-        sum += n;
-        println!("cur sum is {}", sum);
+    let tok = Node::Token {
+        kind: TokenKind::TkNum,
+        next: Box::new(Node::Nil),
+        val: 1,
+        index: 0,
+        len: 4,
+    };
+
+    if let Node::Token { kind, next, val, index, len } = tok {
+        println!("  add ${val}, %rax\n");
     }
-    println!("finished");
 }
