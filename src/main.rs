00f7276a6bb30f606d8a6effca2be55594af93c6
Di Wu <cs.wudilet@gmail.com>
2024-06-10 18:46:06 +0800

    parser considers precedence for +, -, *, /

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,42 @@ use std::rc::Rc;
 enum TokenKind {
     TkPlus,
     TkMinus,
+    TkMul,
+    TkDiv,
     TkNum,   // Numeric literals
     TkEof,   // End-of-file markers
 }
 
-#[derive(Clone)]
+#[derive(PartialEq, PartialOrd)]
+enum Precedence {
+    Lowest,
+    PlusMinus,
+    MulDiv,
+}
+
+#[derive(Clone, PartialEq)]
 struct Token {
     kind: TokenKind,
     val: i32,
     content: String,
     index: usize,
     len: usize,
+}
+
+impl Token {
+    fn precedence(&self) -> Precedence {
+        match self.kind {
+            TokenKind::TkPlus | TokenKind::TkMinus => {
+                return Precedence::PlusMinus;
+            }
+            TokenKind::TkMul | TokenKind::TkDiv => {
+                return Precedence::MulDiv;
+            }
+            _ => {
+                return Precedence::Lowest;
+            }
+        }
+    }
 }
 
 impl Default for Token {
@@ -28,6 +53,26 @@ impl Default for Token {
             content: "".to_string(),
             index: 0,
             len: 0,
+        }
+    }
+}
+
+#[derive(PartialEq)]
+struct Node {
+    token: Token,
+    lhs: Option<Rc<Node>>,
+    rhs: Option<Rc<Node>>,
+    // @TODO: wrap a single integer as IntegerNode
+    val: i32, // valid only when token is a number
+}
+
+impl Node {
+    fn new(lhs: Node, rhs: Node, token: Token, val: i32) -> Self {
+        Node {
+            token,
+            lhs: Some(lhs.into()),
+            rhs: Some(rhs.into()),
+            val,
         }
     }
 }
@@ -58,12 +103,62 @@ impl Parser {
             error_token(&self.src, self.cur_token(), target)
         }
     }
+
+    // for now, we consider infix expresseions and single number
+    fn parse_expr(&mut self, precedence: Precedence) -> Node {
+        // @Incomplete: for now, we assume the first token is an integer
+        let mut expr: Node = self.parse_integer();
+        loop {
+            if precedence > self.peek_token().precedence() {
+                return expr;
+            }
+            self.next_token();
+            match self.cur_token().kind {
+                TokenKind::TkPlus | TokenKind::TkMinus |
+                TokenKind::TkMul | TokenKind::TkDiv => {
+                    expr = self.parse_infix(expr)
+                },
+                TokenKind::TkNum => {
+                    error_token(&self.src, self.cur_token(), "expect an operator");
+                },
+                TokenKind::TkEof => {
+                    break;
+                },
+            }
+        }
+        expr
+    }
+
+    fn parse_integer(&self) -> Node {
+        if self.cur_token().kind != TokenKind::TkNum {
+            error_token(&self.src, self.cur_token(), "expect a number");
+        }
+        Node {
+            token: self.cur_token().clone(),
+            lhs: None,
+            rhs: None,
+            val: self.cur_token().val,
+        }
+    }
+
+    fn parse_infix(&mut self, lhs: Node) -> Node {
+        let tok = self.cur_token().clone();
+        let p = tok.precedence();
+        self.next_token();
+        Node {
+            token: tok,
+            lhs: Some(lhs.into()),
+            rhs: Some(self.parse_expr(p).into()),
+            val: 0,
+        }
+    }
 }
 
 fn main() {
     // test case
-    let src = String::from("10+1234-2-3423+12312-1223412+11112");
+    let src = String::from("10-1234*2-3423/12312-1223412+11112");
     println!("compiling source code: {src}");
+    println!();
 
     let mut tokens: Vec<Token> = Vec::new();
     let tokens = tokenize(&src);
@@ -72,18 +167,25 @@ fn main() {
         tokens: tokens.clone(),
         cur_index: 0,
     };
+    let res = parser.parse_expr(Precedence::Lowest);
+    show_node(&Some(res.into()));
+    println!();
+    // error_token(&src, &res.token, "expect a number for the first token");
+
     // special case: deal with the first token
     if let TokenKind::TkNum = parser.cur_token().kind {
-        println!("mov ${} %rax", parser.cur_token().val);
+        println!("  .globl main");
+        println!("main:");
+        println!("  mov ${} %rax", parser.cur_token().val);
         parser.next_token();
     } else {
         error_token(&src, parser.cur_token(), "expect a number for the first token");
     }
-    while parser.cur_token().kind != TokenKind::TkEof {
+    loop {
         match parser.cur_token().kind {
             TokenKind::TkPlus => {
                 if let TokenKind::TkNum = parser.peek_token().kind  {
-                    println!("add ${}, %rax", parser.peek_token().val);
+                    println!("  add ${}, %rax", parser.peek_token().val);
                     parser.next_token();
                     parser.next_token();
                 } else {
@@ -92,7 +194,7 @@ fn main() {
             },
             TokenKind::TkMinus => {
                 if let TokenKind::TkNum = parser.peek_token().kind  {
-                    println!("sub ${}, %rax", parser.peek_token().val);
+                    println!("  sub ${}, %rax", parser.peek_token().val);
                     parser.next_token();
                     parser.next_token();
                 } else {
@@ -102,10 +204,35 @@ fn main() {
             TokenKind::TkNum => {
                 error_token(&src, parser.cur_token(), "expect an operator");
             },
-            TokenKind::TkEof => {
-                error_token(&src, parser.cur_token(), "expect an operator");
+            _ => {
+                break;
             },
         }
+    }
+    println!("  ret");
+}
+
+fn show_node(root: &Option<Rc<Node>>) {
+     if let Some(ref rc_root) = root {
+        if rc_root.token.kind == TokenKind:: TkNum {
+            // mid
+            print!("{}", rc_root.token.content);
+            return;
+        }
+        print!("(");
+        // left
+        if rc_root.lhs != None {
+            show_node(&rc_root.lhs);
+        }
+        // mid
+        print!("{}", rc_root.token.content);
+        // right
+        if rc_root.rhs != None {
+            show_node(&rc_root.rhs);
+        }
+        print!(")");
+    } else {
+        print!("No value found");
     }
 }
 
@@ -127,6 +254,16 @@ fn tokenize(src: &String) -> Vec<Token> {
                 index += 1;
             } else if c == '-' {
                 tok.kind = TokenKind::TkMinus;
+                tok.content = String::from(c);
+                tok.len = 1;
+                index += 1; 
+            } else if c == '*' {
+                tok.kind = TokenKind::TkMul;
+                tok.content = String::from(c);
+                tok.len = 1;
+                index += 1; 
+            } else if c == '/' {
+                tok.kind = TokenKind::TkDiv;
                 tok.content = String::from(c);
                 tok.len = 1;
                 index += 1; 
