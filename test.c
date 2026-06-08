113f95cf7a893db04a832257c1954254976d1ed3
cs.wudilet@gmail.com <cs.wudilet@gmail.com>
2026-06-09 00:28:27 +0800

    Code cleanup in test.c

--- test.c
+++ test.c
@@ -1,26 +1,22 @@
 int main() {
     struct st1 {
         int a;
-        int b;
-        char c;
+        char b;
     };
     struct st1 qwer;
     struct st1 *qwer_pointer = &qwer;
     qwer.a = 11;
     qwer.b = 22;
-    qwer.c = 33;
     qwer_pointer.a = 12345;
     printf("qwer.a=%d\n", qwer.a);
     printf("qwer.b=%d\n", qwer.b);
-    printf("qwer.c=%d\n", qwer.c);
 
-    struct {char a; char b;} x[3];
-    char *p=x;
-    x[0].a = 99;
-    printf("x[0].a is %d!!!!\n", x[0].a);
-    foo();
-    printf("value:%d\n", 123);
-    return 0;
+    int i = 0;
+    while (1) {
+        printf("i=%d\n", i);
+        i = i + 1;
+        sleep(1);
+    }
 }
 
 int foo() {
